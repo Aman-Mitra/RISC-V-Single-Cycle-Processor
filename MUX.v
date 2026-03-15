879b9806ddd2be5fd52c378a1c1fed471df8ae7d
x-titan-x <153897258+Aman-Mitra@users.noreply.github.com>
2026-03-15 23:42:53 +0530

    Delete files

--- MUX.v
+++
@@ -1,9 +0,0 @@
-module MUX(input [31:0] A,input [31:0] B, input sel, output reg [31:0] out);
-	always @(*) begin
-		case (sel) 
-			1'b0: out = A;
-			1'b1: out = B;
-			default out = A;
-		endcase		
-	end
-endmodule
